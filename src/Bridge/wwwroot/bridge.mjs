3ae5366401913247f5aac34b45eb95075fa29c3f
Dave <dave.belodri@gmail.com>
2025-06-13 20:50:16 +0200

    create bridge.js in Bridge project to interop with the C# EngineBridge

---
+++ src/Bridge/wwwroot/bridge.mjs
@@ -0,0 +1,80 @@
+import { dotnet as _dotnet } from './_framework/dotnet.js';
+/** @type {import('../../Bridge/types/dotnet.js').dotnet} */
+const dotnet = _dotnet;
+
+/**
+ * @import { MonoConfig, RuntimeAPI,  } from '../../Bridge/types/dotnet.js'
+ */
+
+export default class Bridge {
+    static #CONFIG = {
+        NAMESPACE: "Bridge",
+        CLASS_NAME: "EngineBridge",
+        METHODS_SYNC: [
+            "Tick"
+        ],
+        METHODS_ASYNC: [
+            "GetTickErrorText"
+        ],
+    }
+
+    /** @type {RuntimeAPI} */
+    static #api;
+
+    static #exports;
+
+    /** @type {MonoConfig} */
+    static #monoConfig;
+
+    static #EngineBridge;
+
+    static async initialize() {
+        const {NAMESPACE, CLASS_NAME} = Bridge.#CONFIG;
+        if(Bridge.#api) throw new Error("Bridge has already been initialized.");
+
+        Bridge.#api = await dotnet.create();
+        Bridge.#monoConfig = Bridge.#api.getConfig();
+        Bridge.#exports = await Bridge.#api.getAssemblyExports(Bridge.#monoConfig.mainAssemblyName);
+        Bridge.#EngineBridge = Bridge.#exports[NAMESPACE][CLASS_NAME];
+
+        return true;
+    }
+
+    static callSync(name, ...args) {
+        if(!Bridge.#CONFIG.METHODS_SYNC.includes(name)) {
+            throw new Error(`Method "${name}" is not in the configured list of sync methods.`);
+        }
+
+        const method = Bridge.#getMethod(name);
+        return method(...args);
+    }
+
+    static callAsync(name, ...args) {
+        if(!Bridge.#CONFIG.METHODS_ASYNC.includes(name)) {
+            throw new Error(`Method "${name}" is not in the configured list of async methods.`);
+        }
+
+        const method = Bridge.#getMethod(name);
+        return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    try {
+                        const result = method(...args);
+                        resolve(result);
+                    } catch (err) {
+                        reject(err);
+                    }
+                }, 0);
+            });
+    }
+
+    /**
+     * @param {string} name 
+     * @returns {Function}
+     */
+    static #getMethod(name) {
+        const method = this.#EngineBridge[name];
+        if(!method) throw new Error(`Method "${name}" not found in C# class "${Bridge.#CONFIG.CLASS_NAME}"`);
+        return method;
+    }
+
+}
--- src/WebApp/scripts/main.mjs
+++ src/WebApp/scripts/main.mjs
@@ -1,104 +1,7 @@
-import { dotnet as _dotnet } from '../_framework/dotnet.js';
-/** @type {import('../types/dotnet.d.ts').dotnet} */
-const dotnet = _dotnet;
-
-/**
- * @import { MonoConfig, RuntimeAPI,  } from '../types/dotnet.d.ts'
- */
-
-class Bridge {
-    static CONFIG = Object.freeze({
-        NAMESPACE: "Bridge",
-        CLASS_NAME: "EngineBridge",
-        STRICT_MODE: true,
-        METHODS_SYNC: Object.freeze([
-            "Tick"
-        ]),
-        METHODS_ASYNC: Object.freeze([
-            "GetTickErrorText"
-        ]),
-    });
-
-    /** @type {Bridge} */
-    static #handler;
-
-    static get handler() { return Bridge.#handler; }
-
-    /**
-     * 
-     * @returns {Promise<Bridge>}
-     */
-    static async initialize() {
-        if(Bridge.handler) throw new Error("An instance of BridgeHandler already exists.");
-        const runtimeAPI = await dotnet.create();
-        const config = runtimeAPI.getConfig();
-        const exports = await runtimeAPI.getAssemblyExports(config.mainAssemblyName);
-        const handler = new Bridge(runtimeAPI, config, exports);
-        Bridge.#handler = handler;
-        return Bridge.handler;
-    }
-
-    static checkAsync(methodName) {
-        return Bridge.CONFIG.METHODS_ASYNC.includes(methodName);
-    }
-
-    static callMethod(name, ...args) {
-        return Bridge.#handler.callMethod(name, ...args);
-    }
-
-    /** @type {RuntimeAPI} */
-    #api;
-
-    #exports;
-
-    /** @type {MonoConfig} */
-    #monoConfig;
-
-    /** @type {Map<string, Function>} */
-    #methods = new Map();
-
-    #EngineBridge;
-
-    constructor(runtimeAPI, config, exports) {
-        if(Bridge.handler) throw new Error("An instance of BridgeHandler already exists.");
-        this.#api = runtimeAPI;
-        this.#monoConfig = config;
-        this.#exports = exports;
-
-        const {NAMESPACE, CLASS_NAME} = Bridge.CONFIG;
-        this.#EngineBridge = exports[NAMESPACE][CLASS_NAME];
-    }
-
-    callMethod(name, ...args) {
-        if(Bridge.CONFIG.STRICT_MODE) {
-            const isKnown = Bridge.CONFIG.METHODS_SYNC.includes(name) 
-                || Bridge.CONFIG.METHODS_ASYNC.includes(name);
-            if(!isKnown) throw new Error(`Method "${name}" is not in the configured method list.`);
-        }
-
-        const method = this.#EngineBridge[name];
-        if(!method) throw new Error(`Method "${name}" not found in C# class "${Bridge.CONFIG.CLASS_NAME}"`);
-
-        const isAsync = Bridge.checkAsync(name);
-        if(isAsync) {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    try {
-                        const result = method(...args);
-                        resolve(result);
-                    } catch (err) {
-                        reject(err);
-                    }
-                }, 0);
-            });
-        } else {
-            return method(...args);
-        }
-    }
-}
+import Bridge from '../bridge/bridge.mjs';
 
 async function initialize() {
-    await Bridge.initialize("EngineBridge");
+    await Bridge.initialize();
     await test();
 }
 
@@ -107,18 +10,18 @@ async function test() {
 
     console.log("--- Test Run ---");
 
-    tickErrText = await Bridge.callMethod("GetTickErrorText");
-    console.log(`first InvokeCall: ${tickErrText}`);    // expected ""
+    tickErrText = await Bridge.callAsync("GetTickErrorText");
+    console.log(`Expect: ""; Result: ${tickErrText}`);
 
-    Bridge.callMethod("Tick");
+    Bridge.callSync("Tick");
 
-    tickErrText = await Bridge.callMethod("GetTickErrorText");
-    console.log(`second InvokeCall: ${tickErrText}`); // expected "TESTING"
+    tickErrText = await Bridge.callAsync("GetTickErrorText");
+    console.log(`Expect: "TESTING"; Result: ${tickErrText}`);
 
-    Bridge.callMethod("Tick");
+    Bridge.callSync("Tick");
 
-    tickErrText = await Bridge.callMethod("GetTickErrorText");
-    console.log(`third InvokeCall: ${tickErrText}`); // expected ""
+    tickErrText = await Bridge.callAsync("GetTickErrorText");
+    console.log(`Expect: ""; Result: ${tickErrText}`);
 
     console.log("--- Test Complete ---");
 }
