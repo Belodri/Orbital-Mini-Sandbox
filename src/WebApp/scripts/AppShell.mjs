25b6bc689d2dfdb425a2ae3119d493f4f59be0e0
Dave <dave.belodri@gmail.com>
2025-06-24 01:02:30 +0200

    WebApp: Refactor AppShell to include AppDataManager and CanvasView initialization

--- src/WebApp/scripts/AppShell.mjs
+++ src/WebApp/scripts/AppShell.mjs
@@ -1,5 +1,7 @@
 // @ts-ignore
 import _Bridge from '../bridge/bridge.mjs';     // created during build
+import AppDataManager from './AppDataManager.mjs';
+import CanvasView from './components/CanvasView.mjs';
 import Notifications from './components/Notifications.mjs';
 
 export default class AppShell {
@@ -15,17 +17,55 @@ export default class AppShell {
     /** @type {Notifications} */
     static notifications = new Notifications();
 
+    /** @type {AppDataManager} */
+    static appDataManager;
+
+    /** @type {CanvasView} */
+    static canvasView;
+
     //#endregion
 
     static async initialize() {
-        console.log("Begin initialization.")
+        console.log("Begin initialization.");
 
-        this.log("Initializing Bridge...")
+        this.log("Initializing Bridge...");
         await this.Bridge.initialize(); 
 
+        this.log("Instantiating AppDataManager...");
+        this.appDataManager = new AppDataManager();
+
+        this.log("Instantiating CanvasView...");
+        this.canvasView = new CanvasView();
+
         if(this.#CONFIG.debugMode) globalThis.AppShell = this;
-        console.log("Initialization complete.")
+        console.log("Initialization complete.");
     }
+
+    //#region Controls
+
+    /**
+     * 
+     * @param {boolean} [force=undefined] 
+     * @returns {void}
+     */
+    static togglePause(force=undefined) {
+        let newState = force === undefined
+            ? !this.canvasView.isPaused
+            : force;
+
+        if(newState) this.canvasView.pause();
+        else this.canvasView.unpause();
+    }
+
+    static createBody() {
+        const id = this.Bridge.createBody();
+        this.appDataManager._onBodyCreated(id);
+        return id;
+    }
+
+    //#endregion
+
+    //#region Utility
 
     /**
      * 
@@ -34,6 +74,11 @@ export default class AppShell {
      * @returns {void}
      */
     static log(msg, data) {
-        if(this.#CONFIG.debugMode) console.log(msg, data);
+        if(this.#CONFIG.debugMode) {
+            if(data) console.log(msg, data);
+            else console.log(msg);
+        }
     }
+
+    //#endregion
 }
