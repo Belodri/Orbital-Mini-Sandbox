f96eb4361004659e338680ba1fe056dc02db27c8
Dave <dave.belodri@gmail.com>
2025-07-18 22:02:12 +0200

    Physics: Refactor CelestialBody to simplify state management and update logic, and add Acceleration property
    Simplifies the CelestialBody class by replacing manual backing fields and complex update helpers with auto-properties and modern C# patterns.
    
    - Replaced the generic `Updated` event with a specific `EnabledChanged` event, with logic encapsulated in the property setter.
    - Removed the `TryUpdateProperty` and `UpdateNotificationWorker` methods.
    - Added the `Acceleration` property to the body's state.

--- src/Physics/Bodies/CelestialBody.cs
+++ src/Physics/Bodies/CelestialBody.cs
@@ -30,18 +30,20 @@ internal interface ICelestialBody
     Vector2D Velocity { get; }
 
     /// <summary>
-    /// An event raised after the body's state has been successfully changed via the <see cref="Update"/> method.
+    /// The acceleration vector of the body.
+    /// </summary>
+    Vector2D Acceleration { get; }
+
+    /// <summary>
+    /// An event raised after the body's Enabled status has changed.
     /// </summary>
     /// <remarks>
-    /// This event only fires if at least one property's value is different after the update.
-    /// The event handler receives two arguments: the instance of the body that was updated, and a <see cref="BodyDataUpdates"/>
-    /// record containing only the new values for the properties that were changed. Unchanged properties will be <c>null</c> in this record.
+    /// This event only fires if the property's value is different after the update.
     /// </remarks>
-    event Action<ICelestialBody, BodyDataUpdates>? Updated;
+    event Action<ICelestialBody>? EnabledChanged;
 
     /// <summary>
     /// Atomically updates one or more properties of the celestial body.
-    /// If any property's value changes, the <see cref="Updated"/> event is raised.
     /// Unspecified (<c>null</c>) parameters will be ignored and their corresponding properties will remain unchanged.
     /// </summary>
     /// <param name="enabled">The new value for the <see cref="Enabled"/> property. If null, the current value is not changed.</param>
@@ -50,23 +52,29 @@ internal interface ICelestialBody
     /// <param name="posY">The new Y component for the <see cref="Position"/> vector. If null, the Y component is not changed.</param>
     /// <param name="velX">The new X component for the <see cref="Velocity"/> vector. If null, the X component is not changed.</param>
     /// <param name="velY">The new Y component for the <see cref="Velocity"/> vector. If null, the Y component is not changed.</param>
+    /// <param name="accX">The new X component for the <see cref="Acceleration"/> vector. If null, the X component is not changed.</param>
+    /// <param name="accY">The new Y component for the <see cref="Acceleration"/> vector. If null, the Y component is not changed.</param>
     void Update(
         bool? enabled = null,
         double? mass = null,
         double? posX = null,
         double? posY = null,
         double? velX = null,
-        double? velY = null
+        double? velY = null,
+        double? accX = null,
+        double? accY = null
     );
 
     /// <inheritdoc cref="Update(bool?, double?, double?, double?, double?, double?)"/>
     /// <param name="position">The new value for the <see cref="Position"/> vector. If null, the current value is not changed.</param>
     /// <param name="velocity">The new value for the <see cref="Velocity"/> vector. If null, the current value is not changed.</param>
+    /// <param name="acceleration">The new value for the <see cref="Acceleration"/> vector. If null, the current value is not changed.</param>
     void Update(
         bool? enabled = null,
         double? mass = null,
         Vector2D? position = null,
-        Vector2D? velocity = null
+        Vector2D? velocity = null,
+        Vector2D? acceleration = null
     );
 }
 
@@ -79,21 +87,23 @@ internal class CelestialBody : ICelestialBody
         bool enabled = false,
         double mass = 0.0,
         Vector2D? position = null,
-        Vector2D? velocity = null)
+        Vector2D? velocity = null,
+        Vector2D? acceleration = null)
     {
         if (id < 0) throw new ArgumentOutOfRangeException(nameof(id), "Id must not be negative.");
         Id = id;
-        _enabled = enabled;
-        _mass = mass;
+        Enabled = enabled;
+        Mass = mass;
 
         if (position == null)
         {
             var offset = id * INITIALIZATION_POSITION_OFFSET + INITIALIZATION_POSITION_OFFSET;
-            _position = new(offset, offset);
+            Position = new(offset, offset);
         }
-        else _position = position.Value;
+        else Position = position.Value;
 
-        _velocity = velocity ?? Vector2D.Zero;
+        Velocity = velocity ?? Vector2D.Zero;
+        Acceleration = acceleration ?? Vector2D.Zero;
     }
 
     #endregion
@@ -111,29 +121,36 @@ internal class CelestialBody : ICelestialBody
 
     #region Fields & Properties
 
-    bool _enabled;
-    double _mass;
-    Vector2D _position;
-    Vector2D _velocity;
-
     /// <inheritdoc />
     public int Id { get; init; }
     /// <inheritdoc />
-    public bool Enabled { get => _enabled; private set => _enabled = value; }
+    public bool Enabled
+    {
+        get;
+        private set
+        {
+            if (value != field)
+            {
+                field = value;
+                EnabledChanged?.Invoke(this);
+            }
+        }
+    }
     /// <inheritdoc />
-    public double Mass { get => _mass; private set => _mass = value; }
+    public double Mass { get; private set; }
     /// <inheritdoc />
-    public Vector2D Position { get => _position; private set => _position = value; }
+    public Vector2D Position { get; private set; }
     /// <inheritdoc />
-    public Vector2D Velocity { get => _velocity; private set => _velocity = value; }
+    public Vector2D Velocity { get; private set; }
+    /// <inheritdoc />
+    public Vector2D Acceleration { get; private set; }
 
     #endregion
 
+    /// <inheritdoc />
+    public event Action<ICelestialBody>? EnabledChanged;
 
     #region Updates
-
-    /// <inheritdoc />
-    public event Action<ICelestialBody, BodyDataUpdates>? Updated;
 
     /// <inheritdoc />
     public void Update(
@@ -142,13 +159,15 @@ internal class CelestialBody : ICelestialBody
         double? posX = null,
         double? posY = null,
         double? velX = null,
-        double? velY = null)
+        double? velY = null,
+        double? accX = null,
+        double? accY = null)
     {
-        var enabledChanged = TryUpdateProperty(ref _enabled, enabled);
-        var massChanged = TryUpdateProperty(ref _mass, mass);
-        var posChanged = TryUpdateProperty(ref _position, posX, posY);
-        var velChanged = TryUpdateProperty(ref _velocity, velX, velY);
-        UpdateNotificationWorker(enabledChanged, massChanged, posChanged, velChanged);
+        Enabled = enabled ?? Enabled;
+        Mass = mass ?? Mass;
+        Position = Position.With(posX, posY);
+        Velocity = Velocity.With(velX, velY);
+        Acceleration = Acceleration.With(accX, accY);
     }
 
     /// <inheritdoc />
@@ -156,72 +175,14 @@ internal class CelestialBody : ICelestialBody
         bool? enabled = null,
         double? mass = null,
         Vector2D? position = null,
-        Vector2D? velocity = null)
+        Vector2D? velocity = null,
+        Vector2D? acceleration = null)
     {
-        var enabledChanged = TryUpdateProperty(ref _enabled, enabled);
-        var massChanged = TryUpdateProperty(ref _mass, mass);
-        var posChanged = TryUpdateProperty(ref _position, position);
-        var velChanged = TryUpdateProperty(ref _position, position);
-        UpdateNotificationWorker(enabledChanged, massChanged, posChanged, velChanged);
-    }
-
-    /// <summary>
-    /// Notifies the subscribers of this body's update event based on the fields that were updated.
-    /// </summary>
-    /// <param name="enabledChanged">Was the <see cref="Enabled"/> field updated?</param>
-    /// <param name="massChanged">Was the <see cref="Mass"/> field updated?</param>
-    /// <param name="posChanged">Was the <see cref="Position"/> field or one of its components updated?</param>
-    /// <param name="velChanged">Was the <see cref="Velocity"/> field or one of its components updated?</param>
-    private void UpdateNotificationWorker(bool enabledChanged, bool massChanged, bool posChanged, bool velChanged)
-    {
-        bool anyPropertyChanged = enabledChanged || massChanged || posChanged || velChanged;
-        if (!anyPropertyChanged) return;
-
-        BodyDataUpdates delta = new(
-            Enabled: enabledChanged ? _enabled : null,
-            Mass: massChanged ? _mass : null,
-            PosX: posChanged ? _position.X : null,
-            PosY: posChanged ? _position.Y : null,
-            VelX: velChanged ? _velocity.X : null,
-            VelY: velChanged ? _velocity.Y : null
-        );
-        Updated?.Invoke(this, delta);
-    }
-
-    /// <summary>
-    /// Updates a property if it's different.
-    /// </summary>
-    /// <param name="currentValue">A reference to the field/property being updated.</param>
-    /// <param name="newValue">The potential new value.</param>
-    /// <returns>True if the property was changed, otherwise false.</returns>
-    private static bool TryUpdateProperty<T>(ref T currentValue, T? newValue) where T : struct, IEquatable<T>
-    {
-        if (newValue.HasValue && !newValue.Value.Equals(currentValue))
-        {
-            currentValue = newValue.Value;
-            return true;
-        }
-        return false;
-    }
-
-    /// <summary>
-    /// Updates a Vector2D property from its component parts if it's different.
-    /// </summary>
-    /// <param name="currentValue">A reference to the field/property being updated.</param>
-    /// <param name="newX">The potential new value for the X component.</param>
-    /// <param name="newY">The potential new value for the Y component.</param>
-    /// <returns>True if the property was changed, otherwise false.</returns>
-    private static bool TryUpdateProperty(ref Vector2D currentValue, double? newX, double? newY)
-    {
-        var xChange = newX != null && newX.Value != currentValue.X;
-        var yChange = newY != null && newY.Value != currentValue.Y;
-        if (!xChange && !yChange) return false;
-
-        currentValue = new(
-            newX ?? currentValue.X,
-            newY ?? currentValue.Y
-        );
-        return true;
+        Enabled = enabled ?? Enabled;
+        Mass = mass ?? Mass;
+        Position = Position.With(position);
+        Velocity = Velocity.With(velocity);
+        Acceleration = Acceleration.With(acceleration);
     }
 
     #endregion
