13280097784c6a6c904159c23d127b1689b2bd50
Dave <dave.belodri@gmail.com>
2025-09-09 18:53:17 +0200

    WebApp: Remove initial test UI

--- src/WebApp/scripts/main.mjs
+++ src/WebApp/scripts/main.mjs
@@ -26,29 +26,3 @@ window.addEventListener('unhandledrejection', (event) => {
 window.addEventListener("load", async (event) => {
     await AppShell.initialize();
 });
-
-// TEST
-
-document.getElementById("run-test").addEventListener("click", () => {
-    const iterationEle = /** @type {HTMLInputElement} */ (document.getElementById("test-iterations"));
-    const iterations =  Number(iterationEle.value);
-
-    const growBodiesEle = /** @type {HTMLInputElement} */ (document.getElementById("test-bodies-per-run"));
-    const addBodies = Number(growBodiesEle.value);
-
-    const prev = performance.now();
-    for(let i = 0; i < iterations; i++) {
-        for(let j = 0; j < addBodies; j++) AppShell.createBody();
-        AppShell.Bridge.tickEngine(1);
-    }
-    const post = performance.now();
-    const diff = post - prev;
-
-    const totalBodies = AppShell.Bridge.simState.bodyCount;
-    document.getElementById("test-result").innerHTML = `
-        <p>Total Bodies: ${totalBodies}</p>
-        <p>Diff: ${diff.toPrecision()}</p>
-        <p>Prev: ${prev.toPrecision()}</p>
-        <p>Post: ${post.toPrecision()}</p>
-    `;
-});
