092c760dd85bc7c8bf8fb44d08eac57424bc6937
Dave <dave.belodri@gmail.com>
2025-09-19 02:37:13 +0200

    Bridge: Vite Build & ts declaration generation

---
+++ src/Bridge/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        sourcemap: true,
+        outDir: 'wwwroot',
+        // Do NOT clear the output directory on build, as it contains
+        // the `_framework` directory managed by .NET.
+        emptyOutDir: false,
+        lib: {
+            entry: 'scripts/Bridge.ts',
+            fileName: 'bridge',
+            formats: ['es'],
+        },
+        rollupOptions: {
+            external: [
+                './_framework/dotnet.js'
+            ],
+        },
+    },
+});
--- tools/buildSymlinkTypes.sh
+++ tools/buildSymlinkTypes.sh
@@ -6,21 +6,29 @@ set -e
 # This requires either Administrator rights or Developer Mode to be enabled on Windows.
 export MSYS=winsymlinks:nativestrict
 
-PROJECT_FILE="tools/TsTypeGen/TsTypeGen.csproj"
-SOURCE_BRIDGE_TYPE_FILE="src/Bridge/types/Bridge.d.ts"
-SOURCE_LAYOUT_RECORDS_TYPE_FILE="src/Bridge/types/LayoutRecords.d.ts"
+TYPE_GEN_PROJECT_FILE="tools/TsTypeGen/TsTypeGen.csproj"
+BRIDGE_PATH="src/Bridge"
+BRIDGE_BUILD_TYPES_COMMAND="npm run build:types"
+SOURCE_TYPES_DIR="src/Bridge/types"
 DEST_TYPES_DIR="src/WebApp/types"
 
 echo "Build and symlink TypeScript types..."
 
-# Run the dotnet tool to generate types
-dotnet run --project "$PROJECT_FILE"
+# Run the dotnet tool to generate types.
+dotnet run --project "$TYPE_GEN_PROJECT_FILE"
+
+# Build the types from Bridge.
+(cd "$BRIDGE_PATH" && $BRIDGE_BUILD_TYPES_COMMAND)
 
 # Check if the destination directory exists; if not create it.
 mkdir -p "$DEST_TYPES_DIR"
 
-# Symlink the two source files into the target directory, overriding any existing ones
-ln -sfr "$SOURCE_BRIDGE_TYPE_FILE" "$DEST_TYPES_DIR"
-ln -sfr "$SOURCE_LAYOUT_RECORDS_TYPE_FILE" "$DEST_TYPES_DIR"
+# Symlink all files from the source directory into the target directory, overriding any existing ones.
+for source_file in "$SOURCE_TYPES_DIR"/*; do
+    # Check to prevent an error if the source directory is empty.
+    if [ -e "$source_file" ]; then
+        ln -sfr "$source_file" "$DEST_TYPES_DIR"
+    fi
+done
 
 echo "Build and symlink TypeScript types complete."
