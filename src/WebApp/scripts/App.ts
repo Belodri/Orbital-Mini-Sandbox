1b9bcdd97cd1d701e41bd9c5775c92553ad64169
Dave <dave.belodri@gmail.com>
2025-10-08 03:48:26 +0200

    App: App: Add render logging, fix deleteBody return, & move debug-only properties into __debug__ conditional in init()

--- src/WebApp/scripts/App.ts
+++ src/WebApp/scripts/App.ts
@@ -5,12 +5,11 @@ import UiData from "./UI/UiData";
 import PixiHandler from "./UI/PixiHandler";
 import UiManager from "./UI/UiManager";
 import DeferredResolver from "./utils/DeferredResolver";
-
-const DEBUG_MODE: boolean = true as const;  // TODO: Figure out how to set this during compilation.
+import { log } from "./utils/Logger";
 
 // TODO: Add preset methods
 
-export default class App {
+class App {
     static #instanceField: App;
     static get #instance() {
         if(!App.#instanceField) throw new Error("App has not been initialized.");
@@ -38,9 +37,9 @@ export default class App {
      * Deletes a body from the simulation.  
      * Code is executed immediately and can throw synchronous errors!
      * @param id        The unique ID of the body to delete.
-     * @returns         A promise that resolves resolves at the start of the next post-render phase.
+     * @returns         A promise that resolves resolves at the start of the next post-render phase. `true` if the body was deleted, or `false` if it wasn't found.
      */
-    static async deleteBody(id: BodyId): Promise<void> { this.#instance.#resolver.execute(() => Bridge.deleteBody(id)); }
+    static async deleteBody(id: BodyId): Promise<boolean> { return this.#instance.#resolver.execute(() => Bridge.deleteBody(id)); }
 
     /**
      * Updates the simulation's app data or physics data.  
@@ -76,15 +75,17 @@ export default class App {
     static async init(): Promise<void> {
         if(App.#instanceField) return;
 
+        log("Begin initialization.");
+
         const app = new App();
 
-        await Bridge.initialize(DEBUG_MODE);
+        await Bridge.initialize(__DEBUG__);
 
         await PixiHandler.init({
             preRender: app.#preRender,
             render: app.#render,
             postRender: app.#postRender
-        }, { autoStart: !app.#paused }, DEBUG_MODE);
+        }, { autoStart: !app.#paused }, __DEBUG__);
 
         UiData.init({
             physicsState: Bridge.state,
@@ -94,16 +95,26 @@ export default class App {
         });
 
         Notifications.init();
-        UiManager.init({ isDebug: DEBUG_MODE });
+        UiManager.init();
 
         App.#instanceField = app;
 
-        if(DEBUG_MODE) {
+        if(__DEBUG__) {
             // @ts-ignore
             globalThis.App = App;
+
+            Object.defineProperties(App, {
+                "Bridge": { value: Bridge },
+                "PixiHandler": { value: PixiHandler },
+                "UiData": { value: UiData },
+                "Notifications": { value: Notifications },
+                "appData": { get() { App.#instance.#appData }},
+                "resolver": { get() { App.#instance.#resolver }},
+            });
         }
 
         App.#instance.#startRenderLoop();
+        log("Initialization complete.");
     }
 
     //#endregion
@@ -113,23 +124,26 @@ export default class App {
     #paused: boolean = false;
     #started: boolean = false;
 
-    // Debug only getters
-
-    static get Bridge() { return DEBUG_MODE && App.#instanceField ? Bridge : undefined }
-    static get PixiHandler() { return DEBUG_MODE && App.#instanceField ? PixiHandler : undefined }
-    static get UiData() { return DEBUG_MODE && App.#instanceField ? UiData : undefined }
-    static get Notifications() { return DEBUG_MODE && App.#instanceField ? Notifications : undefined }
-    static get appData() { return DEBUG_MODE && App.#instanceField ? App.#instance.#appData : undefined }
-    static get resolver() { return DEBUG_MODE && App.#instanceField ? App.#instance.#resolver : undefined }
-
-
     //#region Render Loop
+
+    #renderLogCount = 0;
+    get #logNextRender() { return this.#renderLogCount > 0; };
+    
+    static logNextRenders(count: number) {
+        if(count <= 0) return;
+        this.#instance.#renderLogCount = count;
+    }
 
     /** 
      * Orchestrates physics calculations and data transformations.  
      * Called by PixiHandler at the very beginning of a new render frame. 
      */
     #preRender = () => {
+        if(this.#logNextRender) {
+            this.#renderLogCount--;
+            log("preRender");
+        }
+
         try {
             const syncOnly = this.#paused;
             Bridge.tickEngine(syncOnly);
@@ -148,6 +162,7 @@ export default class App {
      * Called by PixiHandler after preRender tasks are completed. 
      */
     #render = () => {
+        if(this.#logNextRender) log("render");
         try {
             UiManager.render(this.#paused);
         } catch(err) {
@@ -160,8 +175,11 @@ export default class App {
      * Called by PixiHandler after rendering is fully complete. 
      */
     #postRender = () => {
+        if(this.#logNextRender) log("postRender");
         try {
             this.#resolver.resolve();
+
+            if(this.#logNextRender) log("postRender: End");
         } catch(err) {
             this.#onFatalError(err, `Fatal Post-Render Error.`);
         }
@@ -190,3 +208,14 @@ export default class App {
         // TODO: Option to reset to last snapshot
     }
 }
+
+namespace App {
+    export let Bridge: typeof import("@bridge").default
+    export let PixiHandler: typeof import("./UI/PixiHandler").default;
+    export let UiData: typeof import("./UI/UiData").default;
+    export let Notifications: typeof import("./UI/Notifications").default;
+    export let appData: AppData;
+    export let resolver: DeferredResolver;
+}
+
+export default App;
